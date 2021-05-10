5a60d0acbf6ee28b1735cff67b60cdf8d5d475df
Ahaan Dabholkar <ahaandabholkar@gmail.com>
2021-05-10 12:26:59 +0530

    configuring logger

---
+++ __init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+#       _         _ _             
+#   ___|_|___ ___| |_|___ ___ ___ 
+#  | . | | . | -_| | |   | -_|_ -|
+#  |  _|_|  _|___|_|_|_|_|___|___|
+#  |_|   |_|                      
+
+
+import logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
